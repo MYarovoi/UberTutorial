172a20f2a391f5f220bfb02cec7b9948555652f5
MYarovoi <m.yarovoi96@gmail.com>
2024-08-27 17:54:47 +0300

    Location Input View UI Wrap Up

--- UberTutorial/View/LocationInputView.swift
+++ UberTutorial/View/LocationInputView.swift
@@ -21,10 +21,63 @@ class LocationInputView: UIView {
         let button = UIButton(type: .system)
         button.setImage(UIImage(named: "baseline_arrow_back_black_36dp")?.withRenderingMode(.alwaysOriginal), for: .normal)
         button.addTarget(self, action: #selector(handleBackTapped), for: .touchUpInside)
-        
-        
        return button
     }()
+    
+    private let titleLabel: UILabel = {
+       let label = UILabel()
+        label.text = "Stephan Dowless"
+        label.textColor = .darkGray
+        label.font = .systemFont(ofSize: 16)
+        return label
+    }()
+    
+    private let startLocationIndicatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .lightGray
+        return view
+    }()
+    
+    private let linkingView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .darkGray
+        return view
+    }()
+    
+    private let destinationIndicatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        return view
+    }()
+    
+    private lazy var startingLocationTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Current location"
+        textField.backgroundColor = .systemGray5
+        textField.isEnabled = false
+        textField.font = UIFont.systemFont(ofSize: 14)
+        
+        let paddingView = UIView()
+        paddingView.setDimensions(height: 30, width: 8)
+        textField.leftView = paddingView
+        textField.leftViewMode = .always
+        return textField
+    }()
+    
+    private lazy var destinationLocationTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Enter a destination.."
+        textField.backgroundColor = .lightGray
+        textField.returnKeyType = .search
+        textField.font = UIFont.systemFont(ofSize: 14)
+        
+        let paddingView = UIView()
+        paddingView.setDimensions(height: 30, width: 8)
+        textField.leftView = paddingView
+        textField.leftViewMode = .always
+        return textField
+    }()
+    
     //MARK: Lyfecycle
 
     override init(frame: CGRect) {
@@ -36,6 +89,30 @@ class LocationInputView: UIView {
         
         addSubview(backButton)
         backButton.anchor(top: topAnchor, left: leftAnchor, paddingTop: 44, paddingLeft: 12, width: 24, height: 25)
+        
+        addSubview(titleLabel)
+        titleLabel.centerX(inView: self)
+        titleLabel.centerY(inView: backButton)
+        
+        addSubview(startingLocationTextField)
+        startingLocationTextField.anchor(top: backButton.bottomAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 4, paddingLeft: 40, paddingRight: 40, height: 30)
+        
+        addSubview(destinationLocationTextField)
+        destinationLocationTextField.anchor(top: startingLocationTextField.bottomAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 12, paddingLeft: 40, paddingRight: 40, height: 30)
+        
+        addSubview(startLocationIndicatorView)
+        startLocationIndicatorView.centerY(inView: startingLocationTextField, leftAnchor: leftAnchor, paddingLeft: 20)
+        startLocationIndicatorView.setDimensions(height: 6, width: 6)
+        startLocationIndicatorView.layer.cornerRadius = 6 / 2
+        
+        addSubview(destinationIndicatorView)
+        destinationIndicatorView.centerY(inView: destinationLocationTextField, leftAnchor: leftAnchor, paddingLeft: 20)
+        destinationIndicatorView.setDimensions(height: 6, width: 6)
+        
+        addSubview(linkingView)
+        linkingView.centerX(inView: startLocationIndicatorView)
+        linkingView.anchor(top: startLocationIndicatorView.bottomAnchor, bottom: destinationIndicatorView.topAnchor, paddingTop: 4, paddingBottom: 4, width: 0.5)
+
     }
     
     required init?(coder: NSCoder) {
