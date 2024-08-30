9c58213715119c00cf34cccf980c483d8ee6e76e
MYarovoi <m.yarovoi96@gmail.com>
2024-08-30 14:53:37 +0300

    Populating Cells With Location Data & Custom MKPlacemark Extension

--- UberTutorial/Utils/Extensions.swift
+++ UberTutorial/Utils/Extensions.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import MapKit
 
 extension UIColor {
     
@@ -134,5 +135,18 @@ extension UITextField {
         textField.isSecureTextEntry = isSecureTextEntry
         textField.attributedPlaceholder = NSAttributedString(string: placeholder, attributes: [NSAttributedString.Key.foregroundColor : UIColor.lightGray])
         return textField
+    }
+}
+
+extension MKPlacemark {
+    var address: String? {
+        get {
+            guard let subThoroughfare = subThoroughfare else { return nil }
+            guard let throughfare = thoroughfare else { return nil }
+            guard let locality = locality else { return nil }
+            guard let adminArea = administrativeArea else { return nil }
+            
+            return "\(subThoroughfare) \(throughfare), \(locality), \(adminArea)"
+        }
     }
 }
