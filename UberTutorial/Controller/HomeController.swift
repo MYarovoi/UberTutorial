dbbdb229cec210b2948884c34ccfada2384304a3
MYarovoi <m.yarovoi96@gmail.com>
2024-09-03 15:02:58 +0300

    Animating View To Show/Hide

--- UberTutorial/Controller/HomeController.swift
+++ UberTutorial/Controller/HomeController.swift
@@ -33,6 +33,7 @@ class HomeController: UIViewController {
     private let tableView = UITableView()
     private var searchResults = [MKPlacemark]()
     private final let locationInputViewHeight: CGFloat = 200
+    private final let rideActionViewHeight: CGFloat = 300
     private var actionButtonConfig = ActionButtonConfiguration()
     private var route: MKRoute?
     
@@ -69,6 +70,7 @@ class HomeController: UIViewController {
         UIView.animate(withDuration: 0.3) {
             self.inputActivationView.alpha = 1
             self.configureActionButton(config: .showMenu)
+            self.animateRideActionView(shouldShow: false)
         }
     }
     
@@ -201,7 +203,7 @@ class HomeController: UIViewController {
     
     func configureRideActionView() {
         view.addSubview(rideActionView)
-        rideActionView.frame = CGRect(x: 0, y: view.frame.height - 300, width: view.frame.width, height: 300)
+        rideActionView.frame = CGRect(x: 0, y: view.frame.height, width: view.frame.width, height: rideActionViewHeight)
     }
     
     func configureTableView() {
@@ -223,6 +225,13 @@ class HomeController: UIViewController {
             self.locationInputView.removeFromSuperview()
 
         }, completion: completion)
+    }
+    
+    func animateRideActionView(shouldShow: Bool) {
+        let yOrigin = shouldShow ? self.view.frame.height - self.rideActionViewHeight : self.view.frame.height
+        UIView.animate(withDuration: 0.3) {
+            self.rideActionView.frame.origin.y = yOrigin
+        }
     }
 }
 
@@ -388,6 +397,8 @@ extension HomeController: UITableViewDelegate, UITableViewDataSource {
             
             let annotations = self.mapView.annotations.filter(({ !$0.isKind(of: DriverAnnotation.self) }))
             self.mapView.showAnnotations(annotations, animated: true)
+            
+            self.animateRideActionView(shouldShow: true)
         }
     }
 }
